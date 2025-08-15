ea0c11fae94bce2d3ac9c658137df73c0714220e
Mehari Fitihamlak <meharifitih@gmail.com>
2025-08-15 10:51:44 +0300

    add proto file and unary and impemet unary grpc

---
+++ client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	pb "grpc-demo/proto"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	// conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names:= pb.NameList{
+	// 	Names: []string{"Mehari", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+}
---
+++ client/unary.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	pb "grpc-demo/proto"
+	"log"
+	"time"
+)
+
+func callSayHello(client pb.GreetServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.SayHello(ctx, &pb.NoParam{})
+	if err != nil {
+		log.Fatalf("could not great: %v", err)
+	}
+
+	log.Printf("%s", res.Message)
+}
+ 
